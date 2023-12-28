fa16186623d7b3100563c182e1781e700a58bfed
Tomoe-495 <hasnainsiddique495@gmail.com>
2023-12-28 18:59:58 +0500

    product data view and edit

--- GrandMasala/AdminDashboard.Designer.cs
+++ GrandMasala/AdminDashboard.Designer.cs
@@ -32,7 +32,18 @@
             this.button2 = new System.Windows.Forms.Button();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.button3 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.ProductView = new System.Windows.Forms.DataGridView();
+            this.ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.PRODUCTNAME = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.PRICE = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.QUANTITY = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.CATEGORY = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
             this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ProductView)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
@@ -71,7 +82,7 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(476, 12);
+            this.button3.Location = new System.Drawing.Point(576, 12);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 23);
             this.button3.TabIndex = 2;
@@ -79,20 +90,91 @@
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.ColumnCount = 1;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.Controls.Add(this.ProductView, 0, 0);
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(104, 51);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 1;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(550, 270);
+            this.tableLayoutPanel3.TabIndex = 0;
+            // 
+            // ProductView
+            // 
+            this.ProductView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.ProductView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ID,
+            this.PRODUCTNAME,
+            this.PRICE,
+            this.QUANTITY,
+            this.CATEGORY});
+            this.ProductView.Location = new System.Drawing.Point(3, 3);
+            this.ProductView.Name = "ProductView";
+            this.ProductView.Size = new System.Drawing.Size(544, 263);
+            this.ProductView.TabIndex = 0;
+            this.ProductView.CellEndEdit += new System.Windows.Forms.DataGridViewCellEventHandler(this.ProductView_CellEndEdit);
+            // 
+            // ID
+            // 
+            this.ID.HeaderText = "Id";
+            this.ID.Name = "ID";
+            this.ID.ReadOnly = true;
+            // 
+            // PRODUCTNAME
+            // 
+            this.PRODUCTNAME.HeaderText = "Product Name";
+            this.PRODUCTNAME.Name = "PRODUCTNAME";
+            // 
+            // PRICE
+            // 
+            this.PRICE.HeaderText = "Price";
+            this.PRICE.Name = "PRICE";
+            // 
+            // QUANTITY
+            // 
+            this.QUANTITY.HeaderText = "Quantity";
+            this.QUANTITY.Name = "QUANTITY";
+            // 
+            // CATEGORY
+            // 
+            this.CATEGORY.HeaderText = "Category";
+            this.CATEGORY.Name = "CATEGORY";
+            this.CATEGORY.ReadOnly = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(293, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Products";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
             // AdminDashboard
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(563, 333);
+            this.ClientSize = new System.Drawing.Size(678, 360);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tableLayoutPanel3);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.tableLayoutPanel1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "AdminDashboard";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Dashboard";
             this.Load += new System.EventHandler(this.AdminDashboard_Load);
             this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.ProductView)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -102,5 +184,14 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.DataGridView ProductView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn PRODUCTNAME;
+        private System.Windows.Forms.DataGridViewTextBoxColumn PRICE;
+        private System.Windows.Forms.DataGridViewTextBoxColumn QUANTITY;
+        private System.Windows.Forms.DataGridViewTextBoxColumn CATEGORY;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label label1;
     }
 }
--- GrandMasala/AdminDashboard.cs
+++ GrandMasala/AdminDashboard.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace GrandMasala
 {
@@ -17,9 +18,47 @@ namespace GrandMasala
             InitializeComponent();
         }
 
+        private SqlConnection _sqlconn = new SqlConnection(@"Data Source=LAB5-PC4\SQL2016ENT;Initial Catalog=apnistore;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        private SqlCommand _cmd;
+
         private void AdminDashboard_Load(object sender, EventArgs e)
         {
+            LoadProducts();
+        }
 
+        public void LoadProducts()
+        {
+            try
+            {
+                _sqlconn.Open();
+                _cmd = new SqlCommand("SELECT P.ID, P.NAME, P.PRICE, P.QUANTITY, C.NAME FROM PRODUCT p  INNER JOIN CATEGORY c ON P.CATID = C.ID", _sqlconn);
+                SqlDataReader _reader = _cmd.ExecuteReader();
+
+                // 0 p.id
+                // 1 p.name
+                // 2 p.price
+                // 3 p.quantity
+                // 4 c.name
+                while (_reader.Read())
+                {
+                    ProductView.Rows.Add(new object[] { 
+                        _reader.GetInt32(0), 
+                        _reader.GetString(1), 
+                        _reader.GetDouble(2), 
+                        _reader.GetInt32(3), 
+                        _reader.GetString(4) 
+                    });
+                }
+
+            }
+            catch(Exception err)
+            {
+                MessageBox.Show(err.ToString());
+            }
+            finally
+            {
+                _sqlconn.Close();
+            }
         }
         private void button1_Click(object sender, EventArgs e)
         {
@@ -37,5 +76,35 @@ namespace GrandMasala
         {
             this.Dispose();
         }
+
+        private void ProductView_CellEndEdit(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+                _sqlconn.Open();
+                _cmd = new SqlCommand("UPDATE PRODUCT SET NAME = @NAME, PRICE = @PRICE, QUANTITY = @QUANTITY WHERE ID = @ID", _sqlconn);
+                _cmd.Parameters.AddWithValue("@ID", GetCell(e.RowIndex, 0));
+                _cmd.Parameters.AddWithValue("@NAME", GetCell(e.RowIndex, 1));
+                _cmd.Parameters.AddWithValue("@PRICE", GetCell(e.RowIndex, 2));
+                _cmd.Parameters.AddWithValue("@QUANTITY", GetCell(e.RowIndex, 3));
+                _cmd.ExecuteNonQuery();
+
+                MessageBox.Show("Data Been updated");
+
+            }
+            catch(Exception err) 
+            {
+                MessageBox.Show(err.ToString());
+            }
+            finally
+            {
+                _sqlconn.Close();
+            }
+        }
+        public string GetCell(int row, int column)
+        {
+            return ProductView.Rows[row].Cells[column].Value.ToString();
+        }
+
     }
 }
